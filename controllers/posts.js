ed9b696ae843da347f8cc514702973d4083926e4
kemd <kemd91@gmail.com>
2022-06-01 15:34:21 -0400

    updates delete route

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,20 +2,77 @@ const express = require('express');
 const { userLogin } = require('../authcheck/auth');
 const router = express.Router();
 const Post = require('../models/post');
+// seed data
+const postSeed = require('../models/postSeed');
 
 
-//  ROUTES 
-// /posts/add route  show page 
-router.get('/add', userLogin, (req, res) => {
-    res.render('posts/add');
+// routes 
+
+// Seed routes 
+router.get('/seed', userLogin, (req, res) => {
+    // Post.deleteMany({}, (err, seeds) => {})
+
+    Post.create(postSeed, (err, data) => {
+        res.render('seed', {
+            data
+        })
+    })
 })
 
-// POST route 
+// index route 
+router.get('/', userLogin, (req, res) => {
+    Post.find({}, (err, allPosts) => {
+        res.render('index.ejs', {
+            posts: allPosts
+        });
+    })
+})
+
+//  new route 
+router.get('/new', userLogin, (req, res) => {
+    res.render('new.ejs');
+})
+
+//  delete route 
+router.delete('/:id', userLogin, (req, res) => {
+    Post.findByIdAndDelete(req.params.id, () => {
+        res.redirect('/posts');
+    })
+})
+
+// update route 
+router.put('/:id', userLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    },
+        (err, updatedPost) => {
+            res.redirect(`/posts/${req.params._id}`);
+        })
+})
+
+//  create route 
 router.post('/', userLogin, (req, res) => {
     Post.create(req.body, (err, createdPost) => {
-        req.body.user = req.user._id;
-        res.redirect('/dashboard' + createdPost._id);
-    });
-});
+        res.redirect('/posts');
+    })
+})
+
+//  edit route 
+router.get('/:id/edit', userLogin, (req, res) => {
+    Post.findById(req.params.id, (err, foundPost) => {
+        res.render('edit.ejs', {
+            post: foundPost
+        });
+    })
+})
+
+// show route 
+router.get('/:id', userLogin, (req, res) => {
+    Post.findById(req.params.id, (err, foundPost) => {
+        res.render('show.ejs', {
+            post: foundPost
+        });
+    })
+})
 
 module.exports = router;
